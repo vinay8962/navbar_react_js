5d710a39a425c013bd3ab0de312da45e23493fa5
unknown <vinaykushwah890@gmail.com>
2024-07-24 00:31:39 +0530

    profile card create

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react'
 import Home from './pages/Home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import About from './pages/About'
-import Contact from './pages/Contact'
+
 import Navbar from './Components/Navbar'
 
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Navbar />
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
+
       </Routes>
     </BrowserRouter>
   )
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,19 @@
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 
+
+const icon = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0,
+    fill: "rgba(255, 255, 255, 0)"
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    fill: "rgba(255, 255, 255, 1)"
+  }
+};
 const Navbar = () => {
   const [selectedValue, setSelectedValue] = useState('');
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -78,13 +90,24 @@ const Navbar = () => {
           className="absolute inset-0 flex justify-center h-screen w-full items-center bg-black bg-opacity-50"
           onClick={hideProfile}
         >
-          <div className="bg-white p-8 rounded-lg shadow-lg bg-opacity-50">
-            <h1 className="text-2xl font-bold">Hi, I am User</h1>
+          <div className="w-80 bg-white p-1 rounded-lg shadow-lg bg-opacity-50">
+            <div className="text-center border p-3 border-gray-700 rounded-md">
+              <div className="mb-3  flex justify-center">
+                {/* Admin icon here */}
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-20">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
 
-            <h1>User Name</h1>
-            <p>User Details</p>
 
+
+              </div>
+              <h1 className="text-2xl font-bold my-4">Name</h1>
+              <h3 className="text-xl font-bold my-4">+91-1234567890</h3>
+              <h3 className="text-xl font-bold my-4">email1234@gmail.com</h3>
+              <p className="text-lg font-bold my-4">Address</p>
+            </div>
           </div>
+
         </motion.div>
       )}
 
