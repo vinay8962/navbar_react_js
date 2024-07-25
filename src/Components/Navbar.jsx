d2e27c746199d2cf99d0a73c8c19989eee1c72a8
unknown <vinaykushwah890@gmail.com>
2024-07-25 21:54:23 +0530

    profile card

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,12 @@
 import { motion } from 'framer-motion';
 import React, { useState } from 'react';
+import { FaInstagram, FaFacebookSquare, FaGithub, FaLinkedin } from "react-icons/fa";
+import { RxCross1 } from "react-icons/rx";
+import Avatar from '../assets/portrait-handsome-hipster-man-glasses-3d-rendering.jpg'
 
 
-const icon = {
-  hidden: {
-    opacity: 0,
-    pathLength: 0,
-    fill: "rgba(255, 255, 255, 0)"
-  },
-  visible: {
-    opacity: 1,
-    pathLength: 1,
-    fill: "rgba(255, 255, 255, 1)"
-  }
-};
+
+
 const Navbar = () => {
   const [selectedValue, setSelectedValue] = useState('');
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -84,35 +77,60 @@ const Navbar = () => {
       </div>
       {profile && (
         <motion.div
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -50 }}
+          // initial={{ opacity: 0, y: -50 }}
+          // animate={{ opacity: 1, y: 0 }}
+          // exit={{ opacity: 0, y: -50 }}
           className="absolute inset-0 flex justify-center h-screen w-full items-center bg-black bg-opacity-50"
-          onClick={hideProfile}
+
         >
-          <div className="w-80 bg-white p-1 rounded-lg shadow-lg bg-opacity-50">
-            <div className="text-center border p-3 border-gray-700 rounded-md">
-              <div className="mb-3  flex justify-center">
-                {/* Admin icon here */}
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-20">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                </svg>
+          <motion.div
+            initial={{ opacity: 0, x: 200 }} // Slide in from the right
+            animate={{ opacity: 1, x: 0 }} // Center it
+            transition={{ duration: 1 }}
+            exit={{ opacity: 0, x: 200 }} // Slide out to the right
+            className="w-80   p-1 rounded-lg shadow-lg">
 
-
-
+            <div className="text-center border rounded-lg   border-gray-700 ">
+              <div className='bg-red-100 rounded-t-lg p-2 bg-opacity-50 '>
+                <div className='flex justify-end  '>
+                  <div onClick={hideProfile} >
+                    <motion.div whileHover={{ scale: 1.2, rotate: 90, transition: { duration: 0.5 } }} transition={{ duration: 0.5 }} >
+                      <RxCross1 className='size-5' />
+                    </motion.div>
+                  </div>
+                </div>
+                <div className=" relative top-12  flex justify-center ">
+                  <div className="p-1 border-4 border-black rounded-full">
+                    <img src={Avatar} alt="" className="size-24 rounded-full" />
+                  </div>
+                </div>
               </div>
-              <h1 className="text-2xl font-bold my-4">Name</h1>
-              <h3 className="text-xl font-bold my-4">+91-1234567890</h3>
-              <h3 className="text-xl font-bold my-4">email1234@gmail.com</h3>
-              <p className="text-lg font-bold my-4">Address</p>
+              <div className='bg-white  rounded-b-lg p-2 pt-8 pb-4 bg-opacity-50'>
+                <h1 className="text-2xl font-bold my-4">Name</h1>
+                <h3 className="text-xl font-bold my-4">+91-1234567890</h3>
+                <h3 className="text-xl font-bold my-4">email1234@gmail.com</h3>
+                <p className="text-lg font-bold my-4">Address</p>
+
+
+                <div className='flex justify-center bg-black bg-opacity-50 rounded-full p-2'>
+                  <ul className='flex space-x-6'>
+                    <motion.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }} className='  p-1 rounded-full hover:text-red-600'> <FaInstagram className='size-6' /></motion.li>
+                    <motion.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }} className='  p-1 rounded-full hover:text-blue-700'> <FaFacebookSquare className='size-6' /></motion.li>
+                    <motion.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }} className='  p-1 rounded-full hover:text-black'> <FaGithub className='size-6' /></motion.li>
+                    <motion.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }} className='  p-1 rounded-full hover:text-blue-700'> <FaLinkedin className='size-6' /></motion.li>
+
+                  </ul>
+                </div>
+              </div>
             </div>
-          </div>
+          </motion.div>
 
         </motion.div>
-      )}
+      )
+      }
 
 
-    </header>
+    </header >
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customColor: 'rgba(255, 99, 71, 0.5)', // Example RGB color with opacity
+      },
+    },
   },
   plugins: [],
 }
